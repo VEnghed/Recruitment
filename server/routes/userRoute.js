d8765b4b86d5c20526e8df22922401613879f233
VEnghed <enghed@gmail.com>
2021-02-05 11:23:07 +0100

    conneced layers to make login possible

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
 import controller from '../controller/controller'
 
 const router = express.Router()
@@ -9,8 +10,22 @@ router.post('/register', (req, res) => {
     res.status(200).json({ msg: 'Nice try, Usama', time: new Date().toString() })
 })
 
-router.post('/login', (req, res) => {
-    res.status(200).json({ msg: 'logged in!' })
-})
+router.post('/login',
+    body('username').isAlphanumeric(),
+    body('password').isLength({ min: 4 }),
+    (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        controller.loginUser(req.body)
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch(err => {
+                res.status(401).json(err)
+            })
+    })
 
 export default { router: router, route: ROUTE }
