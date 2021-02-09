c88f4392fe8bfa0fbbbb9e3973db1d31dad3f664
VEnghed <enghed@gmail.com>
2021-02-09 10:48:44 +0100

    testing heroku

--- server/server.js
+++ server/server.js
@@ -6,14 +6,11 @@ import loadHandlers from './routes/index.js'
 
 const app = express()
 app.use(bodyParser.json())
-//app.use(express.static('./build'))
+app.use(express.static('./build'))
 
 // Entry point
 loadHandlers(app)
 
-app.get('/', (req, res) => {
-    res.send('Hello from baba booey')
-})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('Check it out http://localhost:' + (process.env.PORT || 8080))
