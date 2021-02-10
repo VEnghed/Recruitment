0c5bf2c376dd19581ef88f3a05cd707a4a43fb85
erber014 <erikb2@kth.se>
2021-02-10 11:10:28 +0100

    made server work

--- server/server.js
+++ server/server.js
@@ -3,14 +3,24 @@ dotenv.config()
 import express from 'express'
 import bodyParser from 'body-parser'
 import loadHandlers from './routes/index.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express()
 app.use(bodyParser.json())
-//app.use(express.static('./build'))
+app.use(express.static('./build'))
 
 // Entry point
 loadHandlers(app)
 
+
 app.listen(process.env.PORT || 8080, () => {
     console.log('Check it out http://localhost:' + (process.env.PORT || 8080))
 })
+
+app.get('*', function(req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname, '../build/')});
+  });
