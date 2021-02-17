b8e96f49b77e73103032f3ce969187d251d7ccb9
erber014 <erikb2@kth.se>
2021-02-17 12:50:44 +0100

    Change applicationpage to function more like description

--- src/components/applicationpageview/applicationpage.js
+++ src/components/applicationpageview/applicationpage.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 
 
+
 //global arrays use to store user input, probably not a great way to do it
 let competenceArray = [];
 let availabilityArray = [];
@@ -15,7 +16,14 @@ let availabilityArray = [];
 function Applicationpage(props) {
     
     let [competenceList, setCompetenceList] = useState([]);
+    let [comp, set_comp] = useState("");
+    let [yearsexp, set_yearsexp] = useState("");
+    
     let [availabilityList, setAvailabilityList] = useState([]);
+    let [from_date, set_from_date] = useState("");
+    let [to_date, set_to_date] = useState("");
+    
+    //For showing success- and error-messages
     let [msgToUser, setMsgToUser] = useState("");
     
     /**
@@ -72,130 +80,90 @@ function Applicationpage(props) {
     }
 
     /**
-     * @description function for adding a react component called competence
-     * @param prop Used to send variables to this component that can be used inside it
-     */
-    const Competence = (prop) => {
-        const [competence, setCompetence] = useState('')
-        const [yearsExperience, setYearsExperience] = useState('')
-        /**
-         * 
-         * @param {*} stateVal Value in the state
-         * @param {*} identifier The row that should be changed in the array 
-         * @param {*} comp The competence
-         * @param {*} exp Years experience
-         */
-        function changeCompetence(stateVal, identifier, comp, exp) {
-            setCompetence(stateVal);
-            changeCompArray(identifier, comp, exp);
-        }
-        
-        /**
-         * 
-         * @param {*} stateVal Value in the state
-         * @param {*} identifier The row that should be changed in the array 
-         * @param {*} comp The competence
-         * @param {*} exp The experience
-         */
-        function changeYearsExperience(stateVal, identifier, comp, exp) {
-            setYearsExperience(stateVal);
-            changeCompArray(identifier, comp, exp);
-        }
-        return (
-            <div className="Competence">
-                <p>Choose competence:</p>
-                    <select value={competence} 
-                        onChange={event => changeCompetence(event.target.value, 
-                                                        prop.identifier, 
-                                                        event.target.value, 
-                                                        yearsExperience)} 
-                    name="competency-select" className="competency-select">
-                        <option value="">--Välj ett alternativ--</option>
-                        {/*Remove hardcoded alternatives, fetch from database */}
-                        <option value="Korvgrillning">Korvgrillning</option>
-                        <option value="Städning">Städning</option>
-                    </select>
-                    <div className="yearsExperience">
-                        <p>Years of experience</p>
-                        {/* Ensure that only numbers can be entered here! */}
-                        <input value={yearsExperience} type="number" className="experience" 
-                            onChange={event => changeYearsExperience(event.target.value, 
-                                                                    prop.identifier, 
-                                                                    competence, 
-                                                                    event.target.value)}>
-                        </input>
-                    </div>
-            </div>
-        );
-    }
-
-    
-    /**
-     * @description Adds another competence component to the page, competence array and competenceList state
-     * @param {*} event The event that triggers this function
-     */
-    const onAddCompetenceClick = event => {
-        //Do not add too many competencies
-        if(competenceList.length <= 2) {
-            const newCompetence = <Competence identifier={competenceList.length} key={competenceList.length}/>
-            competenceList = [...competenceList, newCompetence]
-            
-            addCompetenceToArr("", "");
-            setCompetenceList(competenceList);
-            //setCompetenceList(competenceList.concat(<Competence key={competenceList.length} />));
-        }        
-    };
-
-    /**
      * @description Function for adding a react component
      * @param prop Used for sending variables that can be used inside this function
      */
-    const Availability = (prop) => {
-        const [availableTo, setAvailableTo] = useState('')
-        const [availableFrom, setAvailableFrom] = useState('')
-        
-        function changeAvFrom(stateVal, identifier, avFrom) {
-            setAvailableFrom(stateVal);
-            changeAvArray(identifier, avFrom, availableTo);
-        }
-        function changeAvTo(stateVal, identifier, avTo) {
-            setAvailableTo(stateVal);
-            changeAvArray(identifier, availableFrom, avTo);
-        }
+    const Av = (prop) => {
+        let from = prop.from_date
+        let to = prop.to_date
         return (
             <div className="Availability">
-                <p>Available from:</p>
-                <input value={availableFrom} 
-                    onChange={event => changeAvFrom(event.target.value, 
-                                                    prop.identifier, 
-                                                    event.target.value)} 
-                    type="date" className="available-from" name="available-from"
-                    min="2021-02-01" max="2030-12-31">
-                </input>
-                    
-                <p>Available to:</p>
-                <input value={availableTo} 
-                    onChange={event => changeAvTo(event.target.value, 
-                                                    prop.identifier, 
-                                                    event.target.value)} 
-                    type="date" className="available-to" name="available-to"
-                    min="2021-02-01" max="2040-12-31">
-                </input>
+                <p>From: {from} To: {to}</p>
             </div>
         );
     };
 
+    /**
+     * @description function for adding a react component called competence
+     * @param prop Used to send variables to this component that can be used inside it
+     */
+    const Comp = (prop) => {
+        let cmp = prop.compet;
+        let yearsexper = prop.exper;
+
+        return (
+            <div className="Competence">
+                <p>Competence: {cmp} Years experience: {yearsexper}</p>
+            </div>
+        );
+    };
+    
     /**
      * @description Adds another availability component to the page, availability array, and availabilyList state
      * @param {*} event The event that triggers this function
      */   
-    const onAddAvailabilityClick = event => {
-        const newAvailability = <Availability identifier={availabilityList.length} key={availabilityList.length}/>
+    function onAddAvailabilityClick() {
+        
+        if(from_date === "" && to_date === "") {
+            setMsgToUser("Please choose both from and to dates")
+            return;
+        }
+        else if(from_date === "") {
+            setMsgToUser("Please choose from date");
+            return;
+        }
+        else if(to_date === "") {    
+            setMsgToUser("Please choose to date");
+            return;
+        }
+        
+        const newAvailability = <Av from_date={from_date} to_date={to_date} identifier={availabilityList.length} key={availabilityList.length}/>
         availabilityList = [...availabilityList, newAvailability]
         setAvailabilityList(availabilityList);
-        addAvailabilityToArr("test", "test")
-    };
+        addAvailabilityToArr(from_date, to_date);
 
+        set_from_date("");
+        set_to_date("");
+        setMsgToUser("");
+    }
+
+
+    /**
+     * @description Adds another competence component to the page, competence array and competenceList state
+     * @param {*} event The event that triggers this function
+     */
+    function onAddCompetenceClick() {
+        if(comp === "" && yearsexp === "") {
+            setMsgToUser("Please choose both competence and years experience")
+            return;
+        }
+        else if(yearsexp === "") {
+            setMsgToUser("Please choose years experience");
+            return;
+        }
+        else if(comp === "") {    
+            setMsgToUser("Please choose competence");
+            return;
+        }
+
+        const newCompetence = <Comp compet={comp} exper={yearsexp} identifier={competenceList.length} key={competenceList.length}></Comp>
+        competenceList = [...competenceList, newCompetence]
+        addCompetenceToArr(comp, yearsexp);
+        setCompetenceList(competenceList);
+        set_comp("");
+        set_yearsexp("");
+        setMsgToUser("");
+    }
 
     /**
      * @description This function sends application data (json format) to the server and handles the server response 
@@ -250,8 +218,29 @@ function Applicationpage(props) {
             pid: 11,
         }
         let applicationData = {availabilityArray, competenceArray, applicant};
+        if (availabilityArray.length === 0 || competenceArray.length === 0) {
+            setMsgToUser("Please add competencies and/or availabilities");
+            return;
+        }
         console.log(applicationData);
         sendApplication(applicationData);
+    }
+
+    /**
+     * @description Reset/delete all data in application by clearing states and arrays.
+     */
+    function onResetApplicationClick() {
+        setCompetenceList([]);
+        set_comp("");
+        set_yearsexp("");
+        setAvailabilityList([]);
+        set_from_date("");
+        set_to_date("");
+        
+        
+        competenceArray = [];
+        availabilityArray = [];
+        setMsgToUser("Application has been reset");
     }
 
     return (
@@ -262,20 +251,72 @@ function Applicationpage(props) {
             </header>
             
             <div id="applicationpage-content">    
-                <p>{msgToUser}</p>
-                <div id="competencies">
-                    <h3>Competencies</h3>
-                    {/* Competence list goes here! */}
-                    {competenceList}
-                    <button  onClick={() => onAddCompetenceClick()}>Add competence</button>
+                <h2>Application</h2>
+                <div id="application">
+                    
+                    <div id="application-items">
+                        <div id="competencies">
+                            <h3>Competencies</h3>
+                            {/* Competence list goes here! */}
+                            {competenceList}
+                        </div>
+                        <div id="availabilities">
+                            <h3>Availabilities</h3>
+                            {availabilityList}
+                        </div>
+                    </div>
+                    <div id="application-inputs">
+                        <div id="competence-stuff">
+                            
+                            <div id="competenceInputs">
+                                <div id="competenceSelector">
+                                    <p>Competence: </p>
+                                    <select value={comp} 
+                                        onChange={event => set_comp(event.target.value)} 
+                                        name="competency-select" className="competency-select">
+                                        <option value="">--Välj ett alternativ--</option>
+                                        {/*Remove hardcoded alternatives, fetch from database */}
+                                        <option value="Korvgrillning">Korvgrillning</option>
+                                        <option value="Städning">Städning</option>
+                                    </select>
+                                </div>
+                                <div id="yearsExperience">
+                                    <p>Years of experience: </p>
+                                    {/* Ensure that only numbers can be entered here! */}
+                                    <input value={yearsexp} type="number" className="experience" 
+                                        onChange={event => set_yearsexp(event.target.value)}>
+                                    </input>
+                                </div>
+                            </div>
+                            <button  onClick={() => onAddCompetenceClick()}>Add competence</button>
+                        </div>
+                        <div id="availability-stuff">
+                            {/*Make sure available-from is before available-to somehow*/}
+
+                            
+                            <div id="availabilitySelect">
+                                <p>Available from:</p>
+                                <input value={from_date} 
+                                    onChange={event => set_from_date(event.target.value)} 
+                                    type="date" className="available-from" name="available-from"
+                                    min="2021-02-01" max="2030-12-31">
+                                </input>
+                                    
+                                <p>Available to:</p>
+                                <input value={to_date} 
+                                    onChange={event => set_to_date(event.target.value)} 
+                                    type="date" className="available-to" name="available-to"
+                                    min="2021-02-01" max="2040-12-31">
+                                </input>
+                            </div>
+                            <button onClick={() => onAddAvailabilityClick()}>Add availability</button>
+                        </div>
+                    </div>
                 </div>
-                <div id="availabilities">
-                    {/*Make sure available-from is before available-to somehow*/}
-                    <h3>Availability</h3>
-                    {availabilityList}
-                    <button onClick={() => onAddAvailabilityClick()}>Add availability</button>
+                <p id="msgToUser">{msgToUser}</p>
+                <div id="reset-application">
+                <button onClick={() => onResetApplicationClick()}>Reset application</button>
                 </div>
-                
                 <div id="send-application">
                     <button onClick={() => onSendApplicationClick()}>Send application</button>
                 </div>
