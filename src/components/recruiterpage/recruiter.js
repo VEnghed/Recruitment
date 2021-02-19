fdcda2e7f698a5c28aa20cd0b523bb71a50d9d61
abyel <Abyel@kth.se>
2021-02-19 15:16:30 +0100

    work on recruiter

--- src/components/recruiterpage/recruiter.js
+++ src/components/recruiterpage/recruiter.js
@@ -13,6 +13,7 @@ function Recruiter() {
     const [timeperiodto, setTimeperiodto] = useState('')
     const [applicationdate, setApplicationdate] = useState('')
     const [competence, setCompetence] = useState('')
+    const [applications, setApplications] = useState([])
     const [errorMsg, setErrormsg] = useState('')
 
     //function for saving user auth token?
@@ -22,31 +23,69 @@ function Recruiter() {
      * given parameters
      */
     function searchApplicants() {
-        if(!name || !timeperiodfrom || !timeperiodto || !applicationdate || !competence) {    //check if all required fields are filled
+        if(!name || !timeperiodfrom || !timeperiodto || !applicationdate || !competence)    //check if all required fields are filled
             return setErrormsg("Please fill in all required fields")
-        }
+        
+        let searchQuery = ({
+            name: name,
+            timeperiodfrom: timeperiodfrom,
+            timeperiodto: timeperiodto,
+            applicationdate: applicationdate,
+            competence: competence
+        })
+
+        fetch('/recruiter/search', {
+            method: 'POST', 
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(searchQuery)
+        }).then(response => {
+            console.log(response)
+            if(response.status === 500)         // internal error
+                setErrormsg(response.statusText)
+            if(response.status === 400)         // bad request
+                setErrormsg(response.statusText)
+            else if(response.status === 201) {  // user is registered 
+                saveResults(response.data)
+            }
+        }) 
     }
 
     //function for creating elements & putting values
+    function saveResults() {
+        let testarray = [
+            {name: "Markus Lövgren", date: "2021-02-07"},
+            {name: "Sarah Brown", date: "2021-01-15"},
+            {name: "Amanuel Isak", date: "2021-02-23"}
+        ]
+        setApplications(testarray)
+    }
 
+
+   
+    let test2 = applications.map(application => (
+        <li key={application.name}>
+            {application.name}
+        </li>
+    ))
+    console.log(test2 );
+        
     return (
         <div className="recruiter-container">
-          <h2 className="register-text" >Recruitment</h2>
+          <h2 className="register-text" >Recruitment</h2> 
           <div className="search-bar">
             <input id="name" type="text" placeholder="Name" value={name} onChange={event => setName(event.target.value)}></input>
             <input id="application-date" type="text" placeholder="Application date" value={applicationdate} onChange={event => setApplicationdate(event.target.value)}></input>
             <input id="time-period-from" type="text" placeholder="From" value={timeperiodfrom} onChange={event => setTimeperiodfrom(event.target.value)}></input>
             <input id="time-period-to" type="text" placeholder="To" value={timeperiodto} onChange={event => setTimeperiodto(event.target.value)}></input>
             <input id="competence" type="text" placeholder="Competence" value={competence} onChange={event => setCompetence(event.target.value)}></input>
-            <button className="search-btn" onClick={() => {}}>Search</button>
+            <button className="search-btn" onClick={() => saveResults()}>Search</button>
           </div>
         <hr></hr>
-        <div className="search-results">
-            <ul>
-                <li>Markus Lövgren - 2021-02-07</li>
-                <li>Sarah Brown - 2021-01-15</li>
-                <li>Amanuel Isak - 2021-02-23</li>
-            </ul>
+        <div className="search-results" >
+        <ul>{test2}</ul>
         </div>
         <button className="load-applicants-btn" onClick={() => {}}>Load more applicants</button>
         </div>
