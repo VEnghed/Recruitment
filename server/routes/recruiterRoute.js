51630223896c1af69fd67fd9e60c72cebcb39cb3
VEnghed <enghed@gmail.com>
2021-03-01 09:11:50 +0100

    hotfix durin presentation

--- server/routes/recruiterRoute.js
+++ server/routes/recruiterRoute.js
@@ -1,35 +1,45 @@
-import express from 'express'
-import controller from '../controller/controller.js'
-import {body , validationResult} from 'express-validator'
+import express from "express";
+import controller from "../controller/controller.js";
+import { body, validationResult } from "express-validator";
+import { authorize } from "./auth/auth.js";
 
-const router = express.Router()
-const ROUTE = '/recruiter'
+const router = express.Router();
+const ROUTE = "/recruiter";
 
-router.post('/search', 
-    body('query.name').notEmpty().isString().isAlpha('en-US',{ignore:" "}),
-    body('query.timeperiodfrom').notEmpty().isString().isDate(),
-    body('query.timeperiodto').notEmpty().isString().isDate(),
-    body('query.competence').notEmpty().isString().isAlpha(),
-    (req, res) => {
-        const error = validationResult(req)
-        if(!error.isEmpty()) {
-            res.statusMessage = 'The following fields are invalid: ';
-            error.array().map(err => res.statusMessage += (err.param) + ', ');
-            return res.status(400).end();  
-        }
+router.post(
+  "/search",
+  authorize,
+  body("query.name").notEmpty().isString().isAlpha("en-US", { ignore: " " }),
+  body("query.timeperiodfrom").notEmpty().isString().isDate(),
+  body("query.timeperiodto").notEmpty().isString().isDate(),
+  body("query.competence").notEmpty().isString().isAlpha(),
+  (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      res.statusMessage = "The following fields are invalid: ";
+      error.array().map((err) => (res.statusMessage += err.param + ", "));
+      return res.status(400).end();
+    }
 
-        //add token validation
-        controller.searchApplications(req.body.query)
-        .then(result => {
-            //console.log(result)
-            res.status(200).json([result]);
-        })
-        .catch((err) => {
-            console.log(err)
-            res.statusMessage = err.msg;
-            res.status(500).end();
-        })
-}) 
+    if (req.role != 1) {
+      res.status(401).send("youre not a recruiter");
+      return;
+    }
+
+    //add token validation
+    controller
+      .searchApplications(req.body.query)
+      .then((result) => {
+        //console.log(result)
+        res.status(200).json([result]);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.statusMessage = err.msg;
+        res.status(500).end();
+      });
+  }
+);
 
 /*
 [
@@ -38,9 +48,8 @@ router.post('/search',
                 {name: "Amanuel Isak", date: "2021-02-23"}
             ]*/
 
+router.get("/details", (req, res) => {
+  res.status(200).json({ msg: "you can get details for applications here" });
+});
 
-router.get('/details', (req, res) => {
-    res.status(200).json({ msg: 'you can get details for applications here' })
-})
-
-export default { router: router, route: ROUTE }
+export default { router: router, route: ROUTE };
